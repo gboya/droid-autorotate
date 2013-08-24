907762c0602746309f8d50c34f19b32b35020e61
Gabriel Boya <gabrielboya@gmail.com>
2013-08-24 17:44:53 +0200

    Added test project

---
+++ Windows/AutoRotate.Tests/SampleTests.fs
@@ -0,0 +1,10 @@
+﻿namespace AutoRotate.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type SampleTests() = 
+    
+    [<TestMethod>]
+    member this.``Test the test configuration``() = 
+        Assert.IsTrue(true)
